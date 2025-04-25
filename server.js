604c5830d0872c8707d3b3e4b124a79009a7b2a1
Rashin18 <135828208+Rashin18@users.noreply.github.com>
2025-04-25 16:18:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -115,3 +115,15 @@ app.get('/api/download/:matchId', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
